e1be612f33eea6b8d84078d165773e0beafaff23
Finn <finn@finn.io>
2016-11-25 09:29:42 -0800

    Add GPL licensing stuff

--- shell.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-from flask import *   # noqa
-from piston import *  # noqa
-
-os.environ['PYTHONINSPECT'] = 'True'
--- shitty-webpack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p static/vendor/bootstrap
-cp -vr node_modules/bootstrap/dist/* piston/static/vendor/bootstrap/
