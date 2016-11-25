5be8ade203cf9a26e6affea1f860d44ca2384971
Finn <finn@finn.io>
2016-11-24 21:59:24 -0800

    Minor GCM improvements

--- piston/notifications/gcm.py
+++ piston/notifications/gcm.py
@@ -11,10 +11,10 @@ def send(subscription):
     gcm_url = "/".join(subscription.get("endpoint").split("/")[:-1])
     gcm_registration = subscription.get("endpoint").split("/")[-1]
     headers = {
-        "User-Agent": "TBD",
+        "User-Agent": db.app.config.get("USER_AGENT"),
         "Authorization": "key=%s" % db.app.config.get("GCM_KEY"),
         "Content-Type": "application/json"
     }
     postdata = json.dumps({"registration_ids": [gcm_registration]})
-    result = requests.post(gcm_url, postdata, headers=headers).json()
+    result = requests.post(gcm_url, postdata, headers=headers).content
     logger.debug("Got response from GCM: %r", result)
